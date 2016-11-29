70b11a787c7e00b6540e16b8cfd39889baa672b6
set2k6 <set2k6@gmail.com>
2016-11-29 13:39:17 -0600

    Add deadstox.xyz to CORS whitelist

--- sneakrs/settings.py
+++ sneakrs/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_REGEX_WHITELIST = ('^(https?://)?ngstox\.herokuapp\.com$', '(localhost|127.0.0.1):8080')
+CORS_ORIGIN_REGEX_WHITELIST = ('^(https?://)?ngstox\.herokuapp\.com$', '(localhost|127.0.0.1):8080', '^(https?://)?(www)?\.deadstox\.xyz$')
 
 ROOT_URLCONF = 'sneakrs.urls'
 
